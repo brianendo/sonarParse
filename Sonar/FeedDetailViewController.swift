4a160e0d1264f5478f973b70d285fe23d8129cd9
Brian Endo <bae46@cornell.edu>
2015-08-19 11:26:54 -0700

    first commit

---
+++ Sonar/FeedDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedDetailViewController.swift
+//  Sonar
+//
+//  Created by Brian Endo on 8/18/15.
+//  Copyright (c) 2015 Brian Endo. All rights reserved.
+//
+
+import UIKit
+
+class FeedDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sonar/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  Sonar
+//
+//  Created by Brian Endo on 8/18/15.
+//  Copyright (c) 2015 Brian Endo. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
